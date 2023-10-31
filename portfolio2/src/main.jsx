4c91290e14390000a210c652d09fb3a3af8c51ce
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-10-31 16:47:54 -0700

    adds components and pages

--- portfolio2/src/main.jsx
+++ portfolio2/src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Error from './pages/Error.jsx'
+import Home from './pages/Home.jsx'
+import Projects from './pages/Projects.jsx'
+import Resume from './pages/Resume.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
 )
---
+++ portfolio2/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>
+                Welcome to my React Portfolio! <br />
+                Utilize the tabs at the top of the screen to navigate to the different sections of this app. <br />
+                This is a single page web application run on React.
+            </p>
+        </div>
+    )
+}
